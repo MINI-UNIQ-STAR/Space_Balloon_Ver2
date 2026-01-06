4cd1ff28bb9f14533a652f3476dd548e1417c7ec
Hyeonsu Park <hyunsu5203@hanmail.net>
2026-01-06 19:07:24 +0900

    feat: Add telemetry module with CRC calculation and data transmission functions.

--- Core/Src/telemetry.c
+++ Core/Src/telemetry.c
@@ -11,8 +11,8 @@ uint16_t CRC16_CCITT(uint8_t *data, uint16_t length) {
     for (uint16_t i = 0; i < length; i++) {
         crc ^= (uint16_t)data[i] << 8;
         for (uint8_t j = 0; j < 8; j++) {
-            if (crc & 0x8000) {
-                crc = (crc << 1) ^ 0x1021;
+            if (crc & 0x8000U) {
+                crc = (crc << 1) ^ 0x1021U;
             } else {
                 crc = (crc << 1);
             }
